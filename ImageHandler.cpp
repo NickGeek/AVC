54d19def6aaa1fb68e28c7ac69d45fa8e2d86da0
Joseph Hong <hongjose@myvuw.ac.nz>
2017-05-05 16:21:27 +1200

    Update ImageHandler.cpp
    This is just a base line for the code for the camera.

---
+++ ImageHandler.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <time.h>
+#include "E101.h"
+
+main(){
+  init();
+  take_picture()
+  left_pixel = get_pixel(80, 120, 3);
+  mid_pixel = get_pixel(160, 120, 3);
+  right_pixel = get_pixel(240, 120, 3);
+}
