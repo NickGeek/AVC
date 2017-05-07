18f415fa85f4ff125a4a05114e477d95570b0ea7
Nick Webster <nick@nick.geek.nz>
2017-05-08 11:21:45 +1200

    Adding some structure for getting the error value

--- Camera.h
+++ Camera.h
@@ -1,14 +1,28 @@
 #include <stdio.h>
 #include <time.h>
-#include <string>
+// #include <string>
 #include "E101.h"
 #include "Sensor.h"
 
 using namespace std;
 
 class Camera: public Sensor {
+	double getErrorValue() {
+		double error = 0;
+		int numWhite = 0;
+
+		//TODO: Calculate error
+
+		if (numWhite > 0) {
+			return error/numWhite;
+		}
+		else {
+			return 0;
+		}
+	}
+
 	public:
-		string getNextDirection() {
+		int[] getNextDirection() {
 			//TODO: Get the next direction using data from the Camera
 			return "";
 		}
