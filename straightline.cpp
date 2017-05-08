f375e17fd2eaa4dacbde3008e2fc4f117c07cdab
Nick Webster <nick@nick.geek.nz>
2017-05-09 11:07:46 +1200

    fixing bugs

--- straightline.cpp
+++ straightline.cpp
@@ -3,18 +3,19 @@
 #include "E101.h"
 
 int main(){
-//This sets up the RPi hardware and ensures 
-//everything is working correctly
-init();
-//Sets the motor correctly to pin 1 to rotate
-//to one direction at MAX speed
-set_motor(1,255);
-sleep(0,500000);
-//Sets the motor correctly to pin 2 to rotate
-//in the same direction where pin 1 is rotating
-set_motor(2,255);
-sleep(0,500000);
-//stops motors
-stop(1);
-stop(2);
-return 0;)
+	//This sets up the RPi hardware and ensures 
+	//everything is working correctly
+	init();
+	//Sets the motor correctly to pin 1 to rotate
+	//to one direction at MAX speed
+	set_motor(1,255);
+	sleep(0,500000);
+	//Sets the motor correctly to pin 2 to rotate
+	//in the same direction where pin 1 is rotating
+	set_motor(2,255);
+	sleep(0,500000);
+	//stops motors
+	stop(1);
+	stop(2);
+	return 0;
+}
