0f244a7acf7a8f5374d2a1814f8172698fd8a26f
Nick Webster <nick@nick.geek.nz>
2017-05-09 11:25:34 +1200

    Adding image testing code to main.cpp

--- Camera.h
+++ Camera.h
@@ -12,6 +12,15 @@ struct Movement {
 };
 
 class Camera: public Sensor {
+	int[] getImageData() {
+		int[3] pixels;
+		take_picture();
+		/** since this is only three pixels from the same line, maybe adding a vertical pixel might be good. */  
+		pixels[0] = get_pixel(80, 120, 3);
+		pixels[1] = get_pixel(160, 120, 3);
+		pixels[3] = get_pixel(240, 120, 3); 
+	}
+
 	double getErrorValue() {
 		double error = 0;
 		int numWhite = 0;
--- main.cpp
+++ main.cpp
@@ -3,5 +3,17 @@
 #include "E101.h"
 
 int main() {
+	init();
+
+	int[3] pixels;
+	take_picture();
+	/** since this is only three pixels from the same line, maybe adding a vertical pixel might be good. */  
+	pixels[0] = get_pixel(80, 120, 3);
+	pixels[1] = get_pixel(160, 120, 3);
+	pixels[3] = get_pixel(240, 120, 3);
+
+	for (int i = 0; i < 3; ++i) {
+		printf("%d\n", pixels[i]);
+	}
 	return 0;
 }
