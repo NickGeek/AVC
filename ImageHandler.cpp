4b59116ecdbaed5f1d8040a66cb043092517bef0
Joseph Hong <hongjose@myvuw.ac.nz>
2017-05-09 12:27:37 +1200

    Update ImageHandler.cpp
    Added a while statement before the if statement to allow the camera to keep taking in pixels all the time.
    This also allows us to turn off the mobility corresponding to the photo pixels (to avoid conflict with IR sensor movements).

--- ImageHandler.cpp
+++ ImageHandler.cpp
@@ -9,36 +9,39 @@ int main() {
 	int sum = 0;
 	/** Location controls the col, pixelH is the variable for the horizontal pixels*/
 	int location, int pixelH, int checkRed, int s;
-	for(location = 0, location < 320, location++){
-	/** since this is only three pixels from the same line, maybe adding a vertical pixel might be good. */  
-		pixelH = get_pixel(location, 120, 3);
-		checkRed = get_pixel(location, 120, 0);
-		if(pixelH > 127){
-			s = 1;
-		}
-		if(checkRed ){ /** finish if statement to make it work*/
-			s = -1;
-		}
-		else{
-			s = 0;
-		}
-		sum = sum + (location - 160) * s;
-		while(true){
-			if(sum < 0){
-				turn_right();
-				println("Turning right"); /** The prints for each if statement is only for testing */
+	while(true){
+		for(location = 0, location < 320, location++){
+		/** since this is only three pixels from the same line, maybe adding a vertical pixel might be good. */  
+			pixelH = get_pixel(location, 120, 3);
+			checkRed = get_pixel(location, 120, 0);
+			if(pixelH > 127){
+				s = 1;
 			}
-			if(sum > 0){
-				turn_left();
-				println("Turning left");
+			if(checkRed ){ /** finish if statement to make it work*/
+				s = -1;
 			}
-			if(sum = 0){
-				drive_forward();
-				println("Moving forward");
+			else{
+				s = 0;
 			}
-			if(sum = -1){
-				/** add code to switch to IR and turn off this camera */
-				println("Red detected, switching to IR sensor");
+			sum = sum + (location - 160) * s;
+			while(true){
+				if(sum < 0){
+					turn_right();
+					println("Turning right"); /** The prints for each if statement is only for testing */
+				}
+				if(sum > 0){
+					turn_left();
+					println("Turning left");
+				}
+				if(sum = 0){
+					drive_forward();
+					println("Moving forward");
+				}
+				if(sum = -1){
+					/** add code to switch to IR and turn off this camera */
+					println("Red detected, switching to IR sensor");
+					return false;
+				}
 			}
 		}
 	}
