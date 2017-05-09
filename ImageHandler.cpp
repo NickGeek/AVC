772a9bc5982e3b6f801094cd17a6ffe82780e7f9
Joseph Hong <hongjose@myvuw.ac.nz>
2017-05-09 12:16:40 +1200

    Update ImageHandler.cpp
    for and while loop base, missing a few bits to make this work.

--- ImageHandler.cpp
+++ ImageHandler.cpp
@@ -5,12 +5,46 @@
 int main() {
 	init();
 	take_picture();
+	display_picture(1,500000);
+	int sum = 0;
+	/** Location controls the col, pixelH is the variable for the horizontal pixels*/
+	int location, int pixelH, int checkRed, int s;
+	for(location = 0, location < 320, location++){
 	/** since this is only three pixels from the same line, maybe adding a vertical pixel might be good. */  
-	left_pixel = get_pixel(80, 120, 3);
-	mid_pixel = get_pixel(160, 120, 3);
-	right_pixel = get_pixel(240, 120, 3); 
+		pixelH = get_pixel(location, 120, 3);
+		checkRed = get_pixel(location, 120, 0);
+		if(pixelH > 127){
+			s = 1;
+		}
+		if(checkRed ){ /** finish if statement to make it work*/
+			s = -1;
+		}
+		else{
+			s = 0;
+		}
+		sum = sum + (location - 160) * s;
+		while(true){
+			if(sum < 0){
+				turn_right();
+				println("Turning right"); /** The prints for each if statement is only for testing */
+			}
+			if(sum > 0){
+				turn_left();
+				println("Turning left");
+			}
+			if(sum = 0){
+				drive_forward();
+				println("Moving forward");
+			}
+			if(sum = -1){
+				/** add code to switch to IR and turn off this camera */
+				println("Red detected, switching to IR sensor");
+			}
+		}
+	}
 }
 
 /** From the lecture, we should stick with using an array to make the robots navigation better.*/
 /** To get past the split paths, we should make the code remember the previous picture it took and what happens to
 that path so if it's a dead end, it can retrace its path to take the other path (but might make coding take longer)*/
+
