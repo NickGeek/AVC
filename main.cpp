afd007874d2a5c9c625ba061fa338ccdaf078488
Nick Webster <nick@nick.geek.nz>
2017-05-16 12:22:39 +1200

    Setting up main.cpp to handle >1 quadrant

--- main.cpp
+++ main.cpp
@@ -2,16 +2,37 @@
 #include <time.h>
 #include "E101.h"
 
+
+
 int main() {
 	init();
-
-	Camera camera;
-	while (true) {
-		Movement m = camera.getNextDirection();
-		set_motor(1, m.motorLeft);
-		set_motor(2, m.motorRight);
-		sleep1(0, 500)
-	}
+	MazeSolver solver;
+	solver.q2();
 
 	return 0;
 }
+
+class MazeSolver {
+	Camera camera;
+public:
+	void q1() {
+		//TODO: Move in a straight line and open gate
+	}
+
+	void q2() {
+		while (true) {
+			Movement m = camera.getNextDirection();
+			set_motor(1, m.motorLeft);
+			set_motor(2, m.motorRight);
+			sleep1(0, 500)
+		}
+	}
+
+	void q3() {
+
+	}
+
+	void q4() {
+
+	}
+};
