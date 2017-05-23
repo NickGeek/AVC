a8834c95852fc908b1d595b31a5222f465a34ae8
Nick Webster <nick@nick.geek.nz>
2017-05-23 16:55:17 +1200

    Syntax fixes

--- IR.cpp
+++ IR.cpp
@@ -5,11 +5,12 @@ class IR: public Sensor {
 		ErrorSignal errorSignal = {0, 0, 0};
 
 		int kp = 0.1;
+		int sensorFront, sensorLeft, sensorRight;
 
 		for (int i = 0; i < 200; i++) {
-			int sensorFront = getRelativeValue(read_analog(0));
-			int sensorLeft = getRelativeValue(read_analog(1));
-			int sensorRight = getRelativeValue(read_analog(7));
+			sensorFront = getRelativeValue(read_analog(0));
+			sensorLeft = getRelativeValue(read_analog(1));
+			sensorRight = getRelativeValue(read_analog(7));
 			sleep1(0, 300);
 		}
 
